b63e65940f0d5d764a10b4aa5550eb7d4a91112e
Lakshya Sharma <lakshyatu@gmail.com>
2021-09-30 12:08:44 +0530

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,10 @@
 import NextAuth from "next-auth";
-import FacebookProvider from "next-auth/providers/facebook";
-import GoogleProvider from "next-auth/providers/google";
-import EmailProvider from "next-auth/providers/email";
-import GitHubProvider from "next-auth/providers/github";
+import Provider from "next-auth/providers";
 
 export default NextAuth({
   providers: [
     // OAuth authentication providers...
-    GitHubProvider({
+    Provider.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
@@ -15,7 +12,7 @@ export default NextAuth({
     //   clientId: process.env.FACEBOOK_ID,
     //   clientSecret: process.env.FACEBOOK_SECRET
     // }),
-    GoogleProvider({
+    Provider.Google({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
       authorizationUrl:
