811070e100e273dbb91f18859ec3dded9e9b3d64
Lakshya Sharma <lakshyatu@gmail.com>
2021-09-30 12:15:19 +0530

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -15,10 +15,6 @@ export default NextAuth({
     Provider.Google({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-      authorizationUrl:
-        "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code",
-      scope:
-        "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/business.manage",
     }),
     // Passwordless / email sign in
     // EmailProvider({
